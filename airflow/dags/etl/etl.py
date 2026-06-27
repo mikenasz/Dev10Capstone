db1d9dde93b0d5009a8aaabb2e9bca4a64d1fb4a
Michael <michael.bienasz@gmail.com>
2026-06-27 00:43:12 -0400

    ETL/Airflow done

---
+++ airflow/dags/etl/etl.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+
+def extract_data(file_path):
+    
+    df = pd.read_csv(file_path)
+    return df 
+
+class Transformer:
+    
+    def __init__(self, ratings_df, results_df):
+        self.ratings_df = ratings_df
+        self.results_df = results_df
+    
+    def drop_columns(self):
+        
+        self.results_df = self.results_df.drop(columns=['city', 'neutral'])
+        self.ratings_df = self.ratings_df.drop(columns=['change'])
+    
+    def drop_nulls(self):
+        
+        self.ratings_df = self.ratings_df.dropna()
+        self.results_df = self.results_df.dropna()
+    
+    def rename_columns(self):
+        
+        self.ratings_df = self.ratings_df.rename(columns={'team': 'country'})
+        self.results_df = self.results_df.rename(columns={'date': 'match_date', 'home_score': 'home_team_score', 'away_score': 'away_team_score', 'country': 'match_location'})
+       
+    def add_winner_column(self):
+        
+        self.results_df['match_winner'] = 'draw'
+        self.results_df.loc[self.results_df['home_team_score'] > self.results_df['away_team_score'], 'match_winner'] = self.results_df['home_team']
+        self.results_df.loc[self.results_df['home_team_score'] < self.results_df['away_team_score'], 'match_winner'] = self.results_df['away_team']
+        
+    def type_cast_columns(self):
+        self.ratings_df['rating'] = self.ratings_df['rating'].astype(int)
+        self.ratings_df['date'] = pd.to_datetime(self.ratings_df['date'], format= 'mixed')
+        self.results_df['home_team_score'] = self.results_df['home_team_score'].astype(int)
+        self.results_df['away_team_score'] = self.results_df['away_team_score'].astype(int)
+        self.results_df['match_date'] = pd.to_datetime(self.results_df['match_date'], format='mixed')
+
+    
+    def transform(self):
+        
+        self.drop_columns()
+        self.drop_nulls()
+        self.rename_columns()
+        self.add_winner_column()
+        self.type_cast_columns()
+        return self.ratings_df, self.results_df
+
+class Loader:
+    
+    def __init__(self, ratings_df, results_df, engine):
+        self.ratings_df = ratings_df
+        self.results_df = results_df
+        self.engine = engine
+    
+    def load_countries_table(self):
+        countries = pd.concat([
+            self.results_df['home_team'],
+            self.results_df['away_team'],
+            self.results_df['match_location'],
+            self.ratings_df['country']
+            ]).unique()
+        countries_df = pd.DataFrame(countries, columns=['country_name'])
+        countries_df.to_sql('countries', self.engine, schema='soccer_schema', if_exists='append', index=False)
+    
+    def load_tournament_table(self):
+        tournaments = self.results_df['tournament'].unique()
+        tournaments_df = pd.DataFrame(tournaments, columns=['tournament_name'])
+        tournaments_df.to_sql('tournaments', self.engine, schema='soccer_schema', if_exists='append', index=False)
+    
+    def load_matches_table(self):
+        countries_df = pd.read_sql('SELECT * FROM soccer_schema.countries', self.engine).set_index('country_name')['country_id']
+        tournaments_df = pd.read_sql('SELECT * FROM soccer_schema.tournaments', self.engine).set_index('tournament_name')['tournament_id']
+        
+        self.results_df['home_team'] = self.results_df['home_team'].map(countries_df)
+        self.results_df['away_team'] = self.results_df['away_team'].map(countries_df)
+        self.results_df['match_location'] = self.results_df['match_location'].map(countries_df)
+        self.results_df['tournament'] = self.results_df['tournament'].map(tournaments_df)
+        self.results_df['match_winner'] = self.results_df['match_winner'].map(countries_df)
+        
+        self.results_df = self.results_df.rename(columns={'home_team': 'home_team_id', 'away_team': 'away_team_id', 'match_location': 'location_country_id', 'tournament': 'tournament_id', 'match_winner': 'winner_id'})
+        self.results_df.to_sql('matches', self.engine, schema='soccer_schema', if_exists='append', index=False)
+        
+    def load_ratings_table(self):
+        countries_df = pd.read_sql('SELECT * FROM soccer_schema.countries', self.engine).set_index('country_name')['country_id']
+        self.ratings_df['country'] = self.ratings_df['country'].map(countries_df)
+        self.ratings_df = self.ratings_df.rename(columns={'country': 'country_id'})
+        self.ratings_df.to_sql('ratings', self.engine, schema='soccer_schema', if_exists='append', index=False)
+        
+    def load_all(self):
+        
+        self.load_countries_table()
+        self.load_tournament_table()
+        self.load_matches_table()
+        self.load_ratings_table()
+    
+    
+         
+    
